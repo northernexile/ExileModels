451d7925eea770e30059202791ddf0fd72fe2664
Allen Hardy <freelance6@jfd.co.uk>
2024-05-24 13:18:10 +0100

    fix migration system

--- src/database/migrations/1716551620741-users.ts
+++ src/database/migrations/1716551620741-users.ts
@@ -1,11 +1,48 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
 
 export class Users1716551620741 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+          new Table({
+              name:'users',
+              columns:[
+                  {
+                      name: 'id',
+                      type: 'uuid',
+                      isPrimary: true,
+                      default: 'uuid_generate_v4()',
+                      isGenerated: true,
+                      generationStrategy: 'uuid',
+                  },
+                  {
+                      name:'name',
+                      type:'varchar'
+                  },
+                  {
+                      name:'email',
+                      type:'varchar'
+                  },
+                  {
+                      name:'password',
+                      type:'varchar'
+                  },
+                  {
+                      name:'createdAt',
+                      type:'varchar'
+                  },
+                  {
+                      name:'updatedAt',
+                      type:'varchar',
+                      isNullable:true
+                  }
+              ],
+          })
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('users');
     }
 
 }
