9a4342b58160f24f2e48076349b6b5b841b42846
Allen Hardy <freelance6@jfd.co.uk>
2024-05-24 15:20:27 +0100

    add user service code

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { TypeOrmModule} from '@nestjs/typeorm';
+import {UserEntity} from '../entities/user.entity';
 
 @Module({
-  providers: [UsersService]
+  providers: [UsersService],
+  exports: [UsersService],
+  imports: [
+    TypeOrmModule.forFeature([UserEntity]),
+  ],
 })
 export class UsersModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository} from '@nestjs/typeorm';
+import { BaseUser} from '../dto/user/base.user';
+import {CreateUserDto} from '../dto/user/create.user';
+import {UserEntity} from '../entities/user.entity';
+import {MongoRepository} from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private userRepository: MongoRepository<UserEntity>,
+  ) { }
+  async findOneBy(email: string): Promise<UserEntity | undefined | null> {
+    return await this.userRepository.findOneBy({ email: email });
+  }
+  async create(createUserDto: CreateUserDto) {
+    return this.userRepository.save({
+      ...createUserDto,
+      createdAt: new Date(),
+    });
+  }
+}
