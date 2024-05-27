1af9c757c6cb6a3fda4f1f505ce7fbcffe1e4231
Allen Hardy <freelance6@jfd.co.uk>
2024-05-27 17:47:10 +0100

    force https

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {SwaggerModule,DocumentBuilder} from '@nestjs/swagger';
+import * as fs from 'fs';
 
 async function bootstrap() {
+  const httpsOptions = {
+    key:fs.readFileSync('./src/cert/key.pem'),
+    cert:fs.readFileSync('./src/cert/cert.pem')
+  }
   const app = await NestFactory.create(AppModule,{
-    snapshot:true
+    snapshot:true,
+    httpsOptions
   });
 
   const config = new DocumentBuilder()
