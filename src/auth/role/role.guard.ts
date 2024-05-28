4b073c9cf10ca1d8f8773e7697515ea89a773a25
Allen Hardy <freelance6@jfd.co.uk>
2024-05-28 11:43:16 +0100

    add a role guard

---
+++ src/auth/role/role.guard.ts
@@ -0,0 +1,25 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { Reflector } from '@nestjs/core';
+
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+  constructor(private reflector:Reflector) {}
+
+  matchRoles(roles: string[], userRoles:string[]):boolean {
+    return roles.some((role) => userRoles.includes(role))
+  }
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const roles = this.reflector.get<string[]>('roles',context.getHandler())
+    if( ! roles) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest()
+    const user = request.user;
+    return this.matchRoles(roles,user.roles)
+  }
+}
