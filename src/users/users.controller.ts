7214ddf2bf1cdd2c666e5e49581c1d640cc8c595
Allen Hardy <freelance6@jfd.co.uk>
2024-05-28 17:38:48 +0100

    add user management routes

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,24 @@
-import { Controller, Get, HttpStatus, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller, Delete,
+  Get,
+  HttpStatus,
+  NotFoundException,
+  Patch,
+  Post, ServiceUnavailableException,
+  UnauthorizedException,
+  UseGuards,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from '../auth/jwt.guard';
 import { RoleGuard } from '../auth/role/role.guard';
-import { ApiBearerAuth, ApiResponse } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiParam, ApiResponse } from '@nestjs/swagger';
 import { Roles } from '../auth/roles/roles.decorator';
 import SuccessResponse from '../services/responses/success.response';
+import userResponse from './user.response';
+import { CreateUserDto } from '../dto/user/create.user';
+import { UpdateUserDto } from '../dto/user/update.user';
+import roleResponse from '../roles/role.response';
 
 @Controller('users')
 export class UsersController {
@@ -24,5 +38,122 @@ export class UsersController {
   @Roles('Admin')
   async findAll() {
     return SuccessResponse('list of users',await this.usersService.findAll())
+  }
+
+  @Get(':userId')
+  @ApiBearerAuth()
+  @ApiResponse({
+    status:HttpStatus.OK,
+    description:'Specific user located by `id`',
+  })
+  @ApiParam({
+    name:'userId',
+    description:'The user id',
+    required:true,
+    type:'Number'
+  })
+  @UseGuards(JwtAuthGuard,RoleGuard)
+  @Roles('Admin')
+  async getById(userId:number) {
+    const user = await this.usersService.findOneById(userId)
+
+    if (user) {
+      return userResponse(HttpStatus.OK,'user found',user)
+    }
+
+    throw new NotFoundException({
+      code:HttpStatus.NOT_FOUND,
+      message:'User not found'
+    })
+  }
+
+  @Post()
+  @ApiBearerAuth()
+  @ApiResponse({
+    status:HttpStatus.CREATED,
+    description:'Create a user'
+  })
+  @UseGuards(JwtAuthGuard,RoleGuard)
+  @Roles('Admin')
+  async create(createUserDto: CreateUserDto) {
+    const user = await this.usersService.create(createUserDto)
+
+    if (! user) {
+      throw new UnauthorizedException()
+    }
+
+    const userInfo = {
+      id:user.id,
+      name:user.username,
+      email:user.email,
+      createdAt:user.createdAt,
+      verifiedAt:user.verifiedAt,
+      updatedAt:user.updatedAt
+    }
+
+    return userResponse(HttpStatus.CREATED,'User created',userInfo)
+  }
+
+  @Patch(':id')
+  @ApiBearerAuth()
+  @ApiResponse({
+    status:HttpStatus.NO_CONTENT,
+    description:'Update a user'
+  })
+  @ApiParam({
+    name:'id',
+    description:'The user id',
+    required:true,
+    type:'Number'
+  })
+  @UseGuards(JwtAuthGuard,RoleGuard)
+  @Roles('Admin')
+  async update(userId:number,@Body() updateUserDto:UpdateUserDto) {
+    const user = await this.usersService.update(userId,updateUserDto)
+
+    if (! user ) {
+      throw new ServiceUnavailableException({
+        code:HttpStatus.SERVICE_UNAVAILABLE,
+        message:'Could not update role' + updateUserDto.username
+      })
+    }
+
+    const userInfo = {
+      id:user.id,
+      name:user.username,
+      email:user.email,
+      createdAt:user.createdAt,
+      verifiedAt:user.verifiedAt,
+      updatedAt:user.updatedAt
+    }
+
+    return userResponse(HttpStatus.NO_CONTENT,'Updated user',userInfo)
+  }
+
+  @Delete(':id')
+  @ApiBearerAuth()
+  @ApiResponse({
+    status:HttpStatus.NO_CONTENT,
+    description:'delete a user'
+  })
+  @ApiParam({
+    name:'id',
+    description:'The user id',
+    required:true,
+    type:'Number'
+  })
+  @UseGuards(JwtAuthGuard,RoleGuard)
+  @Roles('Admin')
+  async delete(userId:number) {
+    const deleted = await this.usersService.delete(userId)
+
+    if(!deleted) {
+      throw new ServiceUnavailableException({
+        code:HttpStatus.SERVICE_UNAVAILABLE,
+        message:'Could not delete user'
+      })
+    }
+
+    return userResponse(HttpStatus.NO_CONTENT,'Deleted user',{})
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,6 +4,7 @@ import {CreateUserDto} from '../dto/user/create.user';
 import {UserEntity} from '../entities/user.entity';
 import {MongoRepository} from 'typeorm';
 import {hashPassword} from '../auth/password.hash';
+import { UpdateUserDto } from '../dto/user/update.user';
 
 @Injectable()
 export class UsersService {
@@ -13,6 +14,31 @@ export class UsersService {
   ) { }
   async findOneBy(email: string): Promise<UserEntity | undefined | null> {
     return await this.userRepository.findOneBy({ email: email });
+  }
+
+  async update(userId:number,payload:UpdateUserDto) {
+    let result:any = false;
+    const user =  this.userRepository.create({
+      updatedAt:new Date(),
+      username:payload.username,
+      email:payload.email
+    })
+    if(await this.userRepository.update(userId,user)) {
+      result = user
+    }
+
+    return result
+  }
+
+  async delete(id:number) {
+    let deleted = false
+    const user = await this.userRepository.findOneBy({id:id});
+    if (user) {
+      await this.userRepository.delete(id)
+      deleted = true
+    }
+
+    return deleted
   }
 
   async findAll() {
