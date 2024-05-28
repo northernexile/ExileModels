2f641245f7c434cff753f976281ba76b382b8fe6
Allen Hardy <freelance6@jfd.co.uk>
2024-05-28 18:11:48 +0100

    get roles for specific user

---
+++ src/users/roles/users.roles.controler.ts
@@ -0,0 +1,42 @@
+import { Controller, Get, HttpStatus, Inject, UseGuards } from '@nestjs/common';
+import { UsersRolesService } from './users.roles.service';
+import { JwtAuthGuard } from '../../auth/jwt.guard';
+import { RoleGuard } from '../../auth/role/role.guard';
+import { ApiBearerAuth, ApiParam, ApiResponse } from '@nestjs/swagger';
+import { Roles } from '../../auth/roles/roles.decorator';
+import SuccessResponse from '../../services/responses/success.response';
+import { RolesService } from '../../roles/roles.service';
+
+@Controller(['user/roles'])
+export class UsersRolesController {
+  constructor(
+    private userRolesService:UsersRolesService,
+    private rolesService:RolesService
+  ) {}
+
+  @UseGuards(JwtAuthGuard,RoleGuard)
+  @Get(':userId/list')
+  @ApiBearerAuth()
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: "List of user roles",
+  })
+  @ApiParam({
+    name:'userId',
+    description:'The user id',
+    required:true,
+    type:'Number'
+  })
+  @UseGuards(JwtAuthGuard)
+  @Roles('Admin','Guest')
+  async findAll(userId:number) {
+    const userRoles = await this.userRolesService.findRolesByUserId(userId);
+    const allRoles = await this.rolesService.findAll()
+    const response = {
+      allRoles:allRoles,
+      userRoles:userRoles
+    }
+
+    return SuccessResponse('Application user roles',response)
+  }
+}
