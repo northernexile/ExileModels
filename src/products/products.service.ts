87dcfc7502ba33f25fee4ffa63d2ada4e853cdae
Allen Hardy <freelance6@jfd.co.uk>
2024-06-04 10:45:47 +0100

    product service delete and find

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,26 +1,42 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { MongoRepository } from 'typeorm';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
+  constructor(
+    @InjectRepository(Product)
+    private productRepository: MongoRepository<Product>,
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
     return 'This action adds a new product';
   }
 
-  findAll() {
+  async findAll() {
     return `This action returns all products`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: number) {
+    return await this.productRepository.findOneBy({ id: id });
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
+  async update(id: number, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: number) {
+    let deleted = false;
+    const product = await this.productRepository.findOneBy({ id: id });
+
+    if (product) {
+      await this.productRepository.delete(id);
+      deleted = true;
+    }
+
+    return deleted;
   }
 }
