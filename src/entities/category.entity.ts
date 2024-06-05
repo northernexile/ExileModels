c36d737b1d18278fe91524c4f7545b5c8d9ea19c
Allen Hardy <freelance6@jfd.co.uk>
2024-06-05 09:41:49 +0100

    fill out entities

--- src/entities/category.entity.ts
+++ src/entities/category.entity.ts
@@ -1,1 +1,15 @@
-export class Category {}
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export class Category {
+  @PrimaryGeneratedColumn({ type: 'bigint' })
+  id: number;
+
+  @Column({ type: 'varchar', nullable: false })
+  name: string;
+
+  @Column({ type: 'timestamp', nullable: false })
+  createdAt: Date;
+
+  @Column({ type: 'timestamp', nullable: true })
+  updatedAt: Date;
+}
