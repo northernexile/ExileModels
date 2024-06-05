520c61dfd8bcd2bd04821feb5e1917095bf3dfa8
Allen Hardy <freelance6@jfd.co.uk>
2024-06-05 09:32:56 +0100

    finish product scale service

--- src/product-scales/product-scales.service.ts
+++ src/product-scales/product-scales.service.ts
@@ -1,26 +1,73 @@
-import { Injectable } from '@nestjs/common';
-import { CreateProductScaleDto } from '../dto/product/scales/create-product-scale.dto'
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { CreateProductScaleDto } from '../dto/product/scales/create-product-scale.dto';
 import { UpdateProductScaleDto } from '../dto/product/scales/update-product-scale.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ProductScale } from 'src/entities/product-scale.entity';
+import { MongoRepository } from 'typeorm';
 
 @Injectable()
 export class ProductScalesService {
+  constructor(
+    @InjectRepository(ProductScale)
+    private productScaleRepository: MongoRepository<ProductScale>,
+  ) {}
   async create(createProductScaleDto: CreateProductScaleDto) {
-    return 'This action adds a new productScale';
+    const created = await this.productScaleRepository.save(
+      createProductScaleDto,
+    );
+
+    if (!created) {
+      throw new UnauthorizedException('Cannot create product scale');
+    }
+
+    return created;
   }
 
   async findAll() {
-    return `This action returns all productScales`;
+    return await this.productScaleRepository.find();
   }
 
   async findOne(id: number) {
-    return `This action returns a #${id} productScale`;
+    const productScale = await this.productScaleRepository.findOneBy({
+      id: id,
+    });
+
+    if (!productScale) {
+      throw new NotFoundException('Product scale not found');
+    }
+
+    return productScale;
   }
 
   async update(id: number, updateProductScaleDto: UpdateProductScaleDto) {
-    return `This action updates a #${id} productScale`;
+    let updated: any = false;
+    const updatedProduct = this.productScaleRepository.create(
+      updateProductScaleDto,
+    );
+
+    updated = await this.productScaleRepository.update(id, updatedProduct);
+
+    if (!updated) {
+      throw new ConflictException('Cannnot update product scale');
+    }
+
+    return updated;
   }
 
   async remove(id: number) {
-    return `This action removes a #${id} productScale`;
+    let deleted: any = false;
+
+    const found = await this.productScaleRepository.findOneBy({ id: id });
+
+    if (found) {
+      deleted = await this.productScaleRepository.delete(id);
+    }
+
+    return deleted;
   }
 }
