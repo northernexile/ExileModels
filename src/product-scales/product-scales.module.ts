6c459f8243ac6a443a3cf6f74f98946a2191d2ca
Allen Hardy <freelance6@jfd.co.uk>
2024-06-05 11:16:10 +0100

    give modules their members

--- src/product-scales/product-scales.module.ts
+++ src/product-scales/product-scales.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProductScalesService } from './product-scales.service';
 import { ProductScalesController } from './product-scales.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductScale } from 'src/entities/product-scale.entity';
 
 @Module({
   controllers: [ProductScalesController],
   providers: [ProductScalesService],
+  exports: [ProductScalesService],
+  imports: [TypeOrmModule.forFeature([ProductScale])],
 })
 export class ProductScalesModule {}
