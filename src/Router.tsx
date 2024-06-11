96604b945272311bd1f9060a016e1843a56bb984
Allen Hardy <freelance6@jfd.co.uk>
2024-06-11 11:59:01 +0100

    some auth action pages

--- src/Router.tsx
+++ src/Router.tsx
@@ -7,6 +7,11 @@ import Blog from './pages/Blog';
 import Contact from './pages/Contact';
 import About from './pages/About';
 import Privacy from './pages/Privacy';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import ResetPassword from './pages/ResetPassword';
+import VerifyRegistration from './pages/VerifyRegistration';
+import VerifyReminder from './pages/VerifyReminder';
 
 export const Router = () => {
     return (
@@ -18,6 +23,11 @@ export const Router = () => {
             <Route path='/contact' element={<Contact />} />
             <Route path='/about' element={<About />} />
             <Route path='/privacy' element={<Privacy />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register/>} />
+            <Route path='/password/rest' element={<ResetPassword/>} />
+            <Route path='/email/verify' element={<VerifyRegistration/>} />
+            <Route path='/reminder/verify' element={<VerifyReminder />} /> 
         </Routes>
     )
 }
