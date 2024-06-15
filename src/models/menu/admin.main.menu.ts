c90f38e6b664c69a010a8d2a2418de44baf25897
Allen Hardy <freelance6@jfd.co.uk>
2024-06-15 11:05:23 +0100

    extend the admin menu

--- src/models/menu/admin.main.menu.ts
+++ src/models/menu/admin.main.menu.ts
@@ -2,7 +2,15 @@ import { MenuItemCollectionInterface } from "./menu.item.collection.interface";
 
 const AdminMainMenu:MenuItemCollectionInterface = {
     items:[
-        {title:'Dashboard',path:'/admin'}
+        {title:'Dashboard',path:'/admin'},
+        {title:'Users',path:'/admin/users'},
+        {title:'Roles',path:'/admin/roles'},
+        {title:'Scales',path:'admin/scales'},
+        {title:'Roles',path:'/admin/roles'},
+        {title:'Models',path:'/admin/models'},
+        {title:'Blog',path:'/admin/blog'},
+        {title:'Contact',path:'/admin/contact'},
+        {title:'Orders',path:'/admin/orders'}
     ]
 }
 
