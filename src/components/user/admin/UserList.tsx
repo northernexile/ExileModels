de4cafc9f69c0daf39652448623e324cd9d5c3bf
Allen Hardy <freelance6@jfd.co.uk>
2024-06-15 12:09:15 +0100

    add user list and connect to api

--- src/components/user/admin/UserList.tsx
+++ src/components/user/admin/UserList.tsx
@@ -1,5 +1,19 @@
+import { useEffect, useState } from "react";
+import { UserApi } from "../../../services/api/user/user.api";
+
+
 
 const UserList = () => {
+    const [users,setUsers] = useState([])
+
+    useEffect(() => {
+        UserApi.list(true).then((users)=>{
+            console.log(users)
+        }).catch((error) =>{
+            console.error(error)
+        })
+    })
+
     return (<>List of users</>)
 }
 
