77bbc235672fa2ac25a98cc3662fbd36f9c24668
Allen Hardy <freelance6@jfd.co.uk>
2024-06-21 16:29:20 +0100

    connect to delete user api

--- src/components/user/admin/user.delete.action.tsx
+++ src/components/user/admin/user.delete.action.tsx
@@ -1,8 +1,11 @@
 import { Button } from "@mui/material";
 import { DataTableColumnInterface } from "../../../models/data/data.table.column.interface";
 import { confirmDialog } from "../../dialog/Confirm";
+import { UserApi } from "../../../services/api/user/user.api";
+
 const renderDeleteButton = (params:any) => {
     const path = '/admin/users/delete/' + params.row.id
+    const id = parseInt(params.row.id)
     return <Button
         variant="contained"
         size="small"
@@ -10,10 +13,19 @@ const renderDeleteButton = (params:any) => {
         onClick={() => {
             console.log(path)
             confirmDialog('Delete user?',()=>
-                console.log('deleting '+path)
+                deleteItem(id)
             )
         }}
     >Delete</Button>
+}
+
+const deleteItem = (id:number) => {
+    const userApi = UserApi;
+
+    userApi.remove(id,true).then(()=>{
+    }).catch(()=>{
+        console.error("Could not remove user");
+    })
 }
 
 export const UserDeleteAction:DataTableColumnInterface  = {
